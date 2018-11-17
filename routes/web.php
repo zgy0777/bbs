607b0d6f111f61a07e5d2946500474af6b283339
zgy0777 <soldier76@qq.com>
2018-11-17 08:01:22 +0000

    Finish user auth

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,27 @@
 //首页
 Route::get('/', 'PagesController@root')->name('root');
 
+
+//Auth::routes();
+// Authentication Routes...
+//登陆视图登陆/登出行为
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+//注册视图注册行为
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+//忘记密码视图
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+//提交邮箱行为
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+//修改密码视图（token验证）
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+//修改密码行为
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
+
