ef5995eac3d0d3f2df028912d89e4cc4d36213b6
zgy0777 <soldier76@qq.com>
2018-11-20 13:38:01 +0000

    Generated topics

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,6 +14,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
+        \App\Models\Topic::class => \App\Policies\TopicPolicy::class,
         \App\Models\User::class => \App\Policies\UserPolicy::class,
     ];
 
