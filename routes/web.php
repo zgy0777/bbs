e8906ddd15b6259ae7099cb773aacc605478b503
zgy0777 <soldier76@qq.com>
2018-11-28 12:57:19 +0000

    User can upload topic image

--- routes/web.php
+++ routes/web.php
@@ -46,3 +46,6 @@ Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'crea
 //话题分类
 Route::resource('categories','CategoriesController',['only' => ['show'] ]);
 
+//富文本上传图片
+Route::post('upload_image','TopicsController@uploadImage')->name('topics.upload_image');
+
