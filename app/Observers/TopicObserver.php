68ba99e49f6fc14522ff5284571352d5859e099e
zgy0777 <soldier76@qq.com>
2018-11-28 14:23:28 +0000

    Prevent XSS injection

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -21,6 +21,9 @@ class TopicObserver
 
     public function saving(Topic $topic)
     {
+        //过滤话题中的body非法标签
+        $topic->body = clean($topic->body,'user_topic_body');
+
         $topic->excerpt = make_excerpt($topic->body);
     }
 
